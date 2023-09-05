f51cfafd3902b7cff60fa4617030c639ae533c00
ekoki <kt.alive.0922@gmail.com>
2023-09-05 17:46:15 +0900

    Add: spinnerの追加

--- src/components/pages/Questions.tsx
+++ src/components/pages/Questions.tsx
@@ -1,12 +1,12 @@
 import { memo, useEffect, useState, VFC } from "react";
-import { Flex, Box, Heading, Divider, Stack, Button } from "@chakra-ui/react"
+import { Flex, Box, Heading, Divider, Stack, Button, Center, Spinner } from "@chakra-ui/react"
 
 import { useQuestions } from "../../hooks/useQuestions";
 import { useMessage } from "../../hooks/useMessage";
 import { Answers } from "./Answers"
 
 export const Questions: VFC = memo(() => {
-  const { getQuestions, questions } = useQuestions();
+  const { getQuestions, questions, loading } = useQuestions();
   const [ currentQuestion, setCurrentQuestion ] = useState<number>(0);
   const [ score, setScore ] = useState<number>(0);
   const [ finished, setFinished ] = useState<boolean>(false);
@@ -33,29 +33,32 @@ export const Questions: VFC = memo(() => {
 
   return (
     <>
-      {finished ? (
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : finished ? (
         <Answers questions={questions} score={score} />
-        ) : (
-          <Flex align="center" justify="center" height="100vh">
-            {questions.length > 0  && (
-              <Box bg="white" w="sm" p={4} borderRadius="3xl" shadow="md">
-                <Heading as="h1" size="lg" textAlign="center">
-                  {questions.length}択クイズ
+      ) : (
+        <Flex align="center" justify="center" height="100vh">
+          {questions.length > 0 && (
+            <Box bg="white" w="sm" p={4} borderRadius="3xl" shadow="md">
+              <Heading as="h1" size="lg" textAlign="center">
+                {questions.length}択クイズ
+              </Heading>
+              <Divider my={1} />
+              <Stack spacing={10} py={10} px={8}>
+                <Heading as="h2" size="md" textAlign="center">
+                  問題{currentQuestion + 1}
                 </Heading>
-                <Divider my={1} />
-                <Stack spacing={10} py={10} px={8}>
-                  <Heading as="h2" size="md" textAlign="center">
-                    問題{currentQuestion + 1}
-                  </Heading>
-                  <p>{questions[currentQuestion].question}</p>
-                  {/* パラメータを渡す関数の呼び出しでアロー関数を使うことにより、レンダリングの際に呼ばれないようにしている。すなわち、アロー関数で記載することにより、ボタンをクリックした際に関数が呼ばれる。 */}
-                  <Button onClick={() => handleQuestion(true, questions[currentQuestion].answer)}>はい</Button>
-                  <Button onClick={() => handleQuestion(false, questions[currentQuestion].answer)}>いいえ</Button>
-                </Stack>
-              </Box>
-            )}
-          </Flex> 
-        )}
+                <p>{questions[currentQuestion].question}</p>
+                <Button onClick={() => handleQuestion(true, questions[currentQuestion].answer)}>はい</Button>
+                <Button onClick={() => handleQuestion(false, questions[currentQuestion].answer)}>いいえ</Button>
+              </Stack>
+            </Box>
+          )}
+        </Flex> 
+      )}
     </>
   )
 });
--- src/hooks/useQuestions.tsx
+++ src/hooks/useQuestions.tsx
@@ -8,16 +8,20 @@ export const useQuestions = () => {
   // useMessageフックからshowMessage関数を取ってきている。
   const { showMessage } = useMessage();
   
+  const [loading, setLoading] = useState(false);
+
   const [questions, setQuestions] = useState<Array<Question>>([]);
 
   // APIからquestionの情報を取得している。
   const getQuestions = useCallback(() => {
+    setLoading(true);
 
     axios.get<Array<Question>>("https://accounting-api-ncq1.onrender.com/index")
     .then((res) => { setQuestions(res.data) })
     .catch(() => { showMessage({ title: "問題の取得に失敗しました", status: "error", duration: 800 })})
+    .finally(() => { setLoading(false)})
   },[]);
 
-  return { getQuestions, questions };
+  return { getQuestions, questions, loading };
 };
 
